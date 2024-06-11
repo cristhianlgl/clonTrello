5f624a736d898a342c975bf5b3fb83b67533b14d
Cristhian <cristhianlgl@hotmail.com>
2024-06-11 17:09:33 -0500

    feat: agregar estilo responsive al login y separar boton como componente

--- src/app/domains/login/pages/login/login.component.ts
+++ src/app/domains/login/pages/login/login.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { BtnComponent } from '@/shared/componets/btn/btn.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [BtnComponent],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
---
+++ src/app/domains/shared/componets/btn/btn.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-btn',
+  standalone: true,
+  imports: [],
+  templateUrl: './btn.component.html',
+})
+export class BtnComponent {
+
+  @Input() typeBtn: 'button' | 'reset' | 'submit' = 'button'
+}
