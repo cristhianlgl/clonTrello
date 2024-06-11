62f4210237172ea24faf3efbcd73bb93e54d74e0
Cristhian <cristhianlgl@hotmail.com>
2024-06-11 17:08:25 -0500

    feat: agregar alias de colores a tailwind y alias a los path de componentes en angular

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors :
+      {
+        primary : colors.blue,
+        success: colors.green 
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
