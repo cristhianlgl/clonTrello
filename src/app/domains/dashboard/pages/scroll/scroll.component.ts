c4c1baaa28e3972060282d6fdbc1180753584a25
Cristhian <cristhianlgl@hotmail.com>
2024-07-19 18:00:09 -0500

    feat: agregar scroll

--- src/app/domains/dashboard/pages/scroll/scroll.component.ts
+++ src/app/domains/dashboard/pages/scroll/scroll.component.ts
@@ -1,12 +1,29 @@
 import { NavbarComponent } from '@/shared/componets/navbar/navbar.component';
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject } from '@angular/core';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { ProductService } from '@/services/product.service';
+import { Product } from '@/models/product.model';
 
 @Component({
   selector: 'app-scroll',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, ScrollingModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './scroll.component.html'
 })
 export class ScrollComponent {
+  productServices = inject(ProductService);
+  products: Product[] = []
 
+  constructor(private cdr: ChangeDetectorRef) {
+  }
+
+  ngOnInit() {
+    this.productServices.getProduct().subscribe({
+      next: (data) => {
+        this.products.push(...data);
+      },
+      complete:() => this.cdr.markForCheck()
+    })    
+  }
 }
---
+++ src/app/domains/services/product.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Product } from '@/models/product.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  private http = inject(HttpClient);
+  constructor() { }
+
+  getProduct() {
+    const url = new URL('https://api.escuelajs.co/api/v1/products?limit=200&offset=5');   
+    return this.http.get<Product[]>(url.href)
+  }
+}
