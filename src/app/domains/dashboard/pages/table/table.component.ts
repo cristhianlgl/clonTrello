7114168fe404445be48f72ca1897930f48206304
Cristhian <cristhianlgl@hotmail.com>
2024-07-23 10:28:10 -0500

    feat: agregar estilo y footer a la tabla

--- src/app/domains/dashboard/pages/table/table.component.ts
+++ src/app/domains/dashboard/pages/table/table.component.ts
@@ -1,26 +1,32 @@
 import { Product } from '@/models/product.model';
 import { ProductService } from '@/services/product.service';
 import { NavbarComponent } from '@/shared/componets/navbar/navbar.component';
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { CdkTableModule } from '@angular/cdk/table';
+import { NgClass } from '@angular/common';
 
 
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [NavbarComponent, CdkTableModule],
+  imports: [NavbarComponent, CdkTableModule, NgClass],
   templateUrl: './table.component.html'
 })
-export class TableComponent implements OnInit {
+export class TableComponent {
 
   productService = inject(ProductService);
   products = signal<Product[]>([]);
+  total = signal(0)
   columns: string[] = ['cover', '#No', 'Name', 'price'];
 
 
   ngOnInit(): void {
     this.productService.getProduct().subscribe({
-      next: (data) => this.products.set(data)
+      next: (data) => {
+        this.products.set(data)
+        let totals =0
+        this.total.set(this.products().reduce((acumulador, product) => acumulador + product.price, 0))
+      }
     })
   }
 
