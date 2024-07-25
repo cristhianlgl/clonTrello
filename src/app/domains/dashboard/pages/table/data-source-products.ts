53a57121e71b719f4c5e243a6d6fc1e9bd8232cf
Cristhian <cristhianlgl@hotmail.com>
2024-07-25 11:21:47 -0500

    feat: agregar filtro a la tabla

--- src/app/domains/dashboard/pages/table/data-source-products.ts
+++ src/app/domains/dashboard/pages/table/data-source-products.ts
@@ -3,33 +3,43 @@ import { DataSource } from "@angular/cdk/collections";
 import { BehaviorSubject, Observable } from "rxjs";
 
 export class ProductDataSource extends DataSource<Product> {
- 
+
     data = new BehaviorSubject<Product[]>([]);
-  
-    init(products: Product[]){
+    dataOriginal: Product[] = [];
+
+    init(products: Product[]) {
         this.data.next(products)
+        this.dataOriginal = products;
     }
 
     connect(): Observable<Product[]> {
-      return this.data;
+        return this.data;
     }
-  
-    disconnect() {}
 
-    getTotal(){
-       return this.data.getValue().reduce((total, product) => total + product.price, 0)
+    disconnect() { }
+
+    getTotal() {
+        return this.data.getValue().reduce((total, product) => total + product.price, 0)
     }
-    updateProduct(id: Product['id'], change: Partial<Product>){
+
+    updateProduct(id: Product['id'], change: Partial<Product>) {
         const products = this.data.getValue();
         const index = products.findIndex(product => product.id === id);
-        if(index < 0)
+        if (index < 0)
             return;
 
         products[index] = {
             ...products[index],
             ...change
         };
-        
+
         this.data.next(products);
     }
-  }
+
+    filter(filtro: string) {
+        var dataFilter = this.dataOriginal
+            .filter(product => `${product.id}${product.title}${product.price}`.toLowerCase()
+                .includes(filtro.toLowerCase()));
+        this.data.next(dataFilter)
+    }
+}
--- src/app/domains/dashboard/pages/table/table.component.ts
+++ src/app/domains/dashboard/pages/table/table.component.ts
@@ -1,17 +1,18 @@
 import { ProductService } from '@/services/product.service';
 import { NavbarComponent } from '@/shared/componets/navbar/navbar.component';
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CdkTableModule } from '@angular/cdk/table';
 import { NgClass } from '@angular/common';
 import { ProductDataSource } from './data-source-products';
 import { BtnComponent } from '@/shared/componets/btn/btn.component';
 import { Product } from '@/models/product.model';
-
+import { ReactiveFormsModule, FormControl } from '@angular/forms';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [NavbarComponent, CdkTableModule, NgClass, BtnComponent],
+  imports: [NavbarComponent, CdkTableModule, NgClass, BtnComponent, ReactiveFormsModule],
   templateUrl: './table.component.html'
 })
 export class TableComponent {
@@ -19,7 +20,9 @@ export class TableComponent {
   productService = inject(ProductService);
   productsDataSource = new ProductDataSource();
   columns: string[] = ['cover', '#No', 'Name', 'price', 'action'];
-
+  filtroInput = new FormControl('',{
+    nonNullable: true
+  })
 
   ngOnInit(): void {
     this.productService.getProduct().subscribe({
@@ -27,10 +30,22 @@ export class TableComponent {
         this.productsDataSource.init(data)
       }
     })
+
+    this.filtroInput.valueChanges
+    .pipe(
+      debounceTime(300)
+    )
+    .subscribe(value =>{
+        this.productsDataSource.filter(value)
+    })
   }
 
   updateProduct(product: Product){
     this.productsDataSource.updateProduct(product.id, {price: 20})
+  }
+
+  filterProduct(){
+    this.productsDataSource.filter(this.filtroInput.value)
   }
 
 }
